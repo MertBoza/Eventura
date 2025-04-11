4c6a9d37d7aa0deed794a5928fd84c76478d678d
MertBoza <mertboza45@gmail.com>
2025-04-11 13:08:35 +0200

    Added user.js and routing

--- controllers/user.js
+++ controllers/user.js
@@ -20,4 +20,51 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await prisma.user.delete({ where: { id: parseInt(id) } });
+    res.send("User deleted");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, email, password, roleId } = req.body;
+    const updatedUser = await prisma.user.update({
+      where: { id: parseInt(id) },
+      data: { firstName, lastName, email, password, roleId },
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+};
+
+module.exports = { createUser, deleteUser, updateUser, getAllUsers, getUser };
--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
-const { createUser } = require("../controllers/user");
+const {
+  createUser,
+  deleteUser,
+  updateUser,
+  getAllUsers,
+  getUser,
+} = require("../controllers/user");
 
 router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
 module.exports = router;
