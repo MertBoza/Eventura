f9bdf417111c816a4d67ebf1029f041f09ea89c8
MertBoza <mertboza45@gmail.com>
2025-06-30 17:38:00 +0200

    Added filtration button for categories

--- frontend/src/app/views/eventgallery/eventgallery.component.ts
+++ frontend/src/app/views/eventgallery/eventgallery.component.ts
@@ -1,27 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventService, Event } from '../services/events.service';
+import { CategoryService, Category } from '../services/category.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';  // For ngModel
 
 @Component({
   selector: 'app-eventgallery',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterModule],
+  imports: [CommonModule, HttpClientModule, RouterModule, FormsModule],
   templateUrl: './eventgallery.component.html',
   styleUrls: ['./eventgallery.component.scss'],
 })
 export class EventGalleryComponent implements OnInit {
   events: Event[] = [];
+  filteredEvents: Event[] = [];
+  categories: Category[] = [];
+  selectedCategoryId: string = '';
   isLoading = true;
   error: string | null = null;
 
-  constructor(private eventService: EventService) {}
+  constructor(
+    private eventService: EventService,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.error = null;
+
+    // Load categories
+    this.categoryService.getAllCategories().subscribe({
+      next: (cats) => {
+        this.categories = cats;
+      },
+      error: (err) => {
+        console.error('Error loading categories:', err);
+      },
+    });
+
+    // Load events
     this.eventService.getAllEvents().subscribe({
       next: (data) => {
         this.events = data;
+        this.applyFilter();
         this.isLoading = false;
       },
       error: (err) => {
@@ -29,5 +52,18 @@ export class EventGalleryComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  onCategoryChange(): void {
+    this.applyFilter();
+  }
+
+  private applyFilter(): void {
+    if (!this.selectedCategoryId) {
+      this.filteredEvents = this.events;
+    } else {
+      const id = Number(this.selectedCategoryId);
+      this.filteredEvents = this.events.filter(event => event.categoryId === id);
+    }
   }
 }
